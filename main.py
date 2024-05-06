99bfafd4c660ba395c17315da2ca511af1137563
schadenfreude <127835625+schadenfreude9@users.noreply.github.com>
2024-05-06 21:06:36 +0200

    test

--- main.py
+++ main.py
@@ -35,8 +35,8 @@ def search_sploit(open_ports):
         product = port[0]
         version = port[1]
         print(f'Searching for exploits for {product} {version}...')
-        result = subprocess.check_output(f'searchsploit {product} {version} --id ', shell=True)
-        print(result)
+        output_bytes = subprocess.check_output(f'searchsploit {product} {version} --id ', shell=True)
+        print(result=output_bytes.decode('utf-8'))
 
 if __name__ == '__main__':
     if len(sys.argv) != 2:
