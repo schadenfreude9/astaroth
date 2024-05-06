60926cca5171e722ac9e130fa5cd5ed45e0d40b5
schadenfreude <127835625+schadenfreude9@users.noreply.github.com>
2024-05-06 19:21:14 +0200

    test

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 # Projet SDV - 2024 Cybersécurité
 # Author DMO, LIT & MNE
 # A python program that use the python-nmap library to scan a host and store the open ports and their versions in an array
-# Each port is an array for which the first element is the port number and the second element is the version
+# Each port is an array for which the first element is the service name and the second element is the version
 # The user call the function scan_host() and pass the host ip address as a parameter
 
 import nmap
@@ -9,14 +9,15 @@ import sys
 
 def scan_host(host):
     nm = nmap.PortScanner()
-    nm.scan(host, '1-10000')
+    nm.scan(host, arguments='-sV')
     open_ports = []
     for host in nm.all_hosts():
         for proto in nm[host].all_protocols():
             lport = nm[host][proto].keys()
             for port in lport:
-                if nm[host][proto][port]['state'] == 'open':
-                    open_ports.append([port, nm[host][proto][port]['version']])
+                service = nm[host][proto][port]['name']
+                version = nm[host][proto][port]['version']
+                open_ports.append([service, version])
     return open_ports
 
 if __name__ == '__main__':
