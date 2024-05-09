dd3368aa8cde1f5d2f413c38f733f32f70d30833
schadenfreude <127835625+schadenfreude9@users.noreply.github.com>
2024-05-09 22:16:09 +0200

    dqsdq

--- main.py
+++ main.py
@@ -46,9 +46,9 @@ def sploit_to_pdf(list_of_sploit):
     default_template="template.pug",
     title="Report d'exploitation de la machine " + host,
     )
-    html = report_writer.pug_to_html(table_defou= list_of_sploit)
+    html = report_writer.pug_to_html("template.pug", table_defou= list_of_sploit)
     report_writer.write_report(html, "report_exploitation.pdf")    
-    
+    v
 if __name__ == '__main__':
     if len(sys.argv) != 2:
         print('Usage: python3 main.py <host>')
